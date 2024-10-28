2a70de08e152741754ffd1069eede27dc20e1305
el1ght <el1ghtifyoucan@gmail.com>
2024-10-28 16:45:10 +0200

    advise retest

--- components/UserDataLayout.tsx
+++ components/UserDataLayout.tsx
@@ -70,10 +70,10 @@ const UserDataLayout = ({
                     </h2>
                 </div>
 
-                <div className="grid grid-rows-8 grid-cols-5 gap-2 h-[100vh]">
+                <div className="grid grid-rows-8 grid-cols-5 gap-2 h-[100vh] box">
                     <LikedWidget name={'Test'} href={'/liked'} />
                     <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden'}>
-                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left"}>
+                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left box"}>
                             <div className={'relative h-full w-full bg-black/[.3]'}>
                                 <p className={'truncate text-white text-left m-4 mb-0 text-sm font-medium'}>Test</p>
                                 <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}></p>
@@ -83,8 +83,8 @@ const UserDataLayout = ({
                             <FaPlay size={14} className={'text-black'} />
                         </div>
                     </div>
-                    <div className={'relative row-span-6 col-span-2 rounded-3xl overflow-hidden'}>
-                        <div className={"group flex items-center h-full bg-cover bg-center bg-[red] overflow-hidden hover:saturate-150 transition"}>
+                    <div className={'relative row-span-6 col-span-2 rounded-3xl overflow-hidden box'}>
+                        <div className={"group flex items-center h-full bg-cover bg-center bg-[red] overflow-hidden hover:saturate-150 transition box"}>
                             <div className={'relative h-full w-full bg-black/[.3] rounded-3xl flex flex-col items-center justify-center'}>
                                 <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-medium'}></p>
                                 <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-medium'}></p>
@@ -92,8 +92,8 @@ const UserDataLayout = ({
                             </div>
                         </div>
                     </div>
-                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden '}>
-                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left"}>
+                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden box'}>
+                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left box"}>
                             <div className={'relative h-full w-full bg-black/[.3]'}>
                                 <p className={'truncate text-white text-left m-4 mb-0 text-sm font-medium'}>Test</p>
                                 <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}></p>
@@ -103,8 +103,8 @@ const UserDataLayout = ({
                             <FaPlay size={14} className={'text-black'} />
                         </div>
                     </div>
-                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden'}>
-                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left"}>
+                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden box'}>
+                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left box"}>
                             <div className={'relative h-full w-full bg-black/[.3]'}>
                                 <p className={'truncate text-white text-left m-4 mb-0 text-sm font-medium'}>Test</p>
                                 <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}></p>
@@ -118,53 +118,8 @@ const UserDataLayout = ({
 
             </div>
         ) : (
-            <div className={'p-6'}>
+            <div>
                 Loading...
-                <div className="grid grid-rows-8 grid-cols-5 gap-2 h-[100vh]">
-                    <LikedWidget name={'Test'} href={'/liked'} />
-                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden'}>
-                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left"}>
-                            <div className={'relative h-full w-full bg-black/[.3]'}>
-                                <p className={'truncate text-white text-left m-4 mb-0 text-sm font-medium'}>Test</p>
-                                <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}></p>
-                            </div>
-                        </div>
-                        <div className={'absolute right-0.5 bottom-0.5 rounded-full p-2'}>
-                            <FaPlay size={14} className={'text-black'} />
-                        </div>
-                    </div>
-                    <div className={'relative row-span-6 col-span-2 rounded-3xl overflow-hidden'}>
-                        <div className={"group flex items-center h-full bg-cover bg-center bg-[red] overflow-hidden hover:saturate-150 transition"}>
-                            <div className={'relative h-full w-full bg-black/[.3] rounded-3xl flex flex-col items-center justify-center'}>
-                                <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-medium'}></p>
-                                <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-medium'}></p>
-                                <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-medium'}></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden '}>
-                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left"}>
-                            <div className={'relative h-full w-full bg-black/[.3]'}>
-                                <p className={'truncate text-white text-left m-4 mb-0 text-sm font-medium'}>Test</p>
-                                <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}></p>
-                            </div>
-                        </div>
-                        <div className={'absolute right-0.5 bottom-0.5 rounded-full p-2'}>
-                            <FaPlay size={14} className={'text-black'} />
-                        </div>
-                    </div>
-                    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden'}>
-                        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[#424242] overflow-hidden hover:saturate-150 transition top-left"}>
-                            <div className={'relative h-full w-full bg-black/[.3]'}>
-                                <p className={'truncate text-white text-left m-4 mb-0 text-sm font-medium'}>Test</p>
-                                <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}></p>
-                            </div>
-                        </div>
-                        <div className={'absolute right-0.5 bottom-0.5 rounded-full p-2'}>
-                            <FaPlay size={14} className={'text-black'} />
-                        </div>
-                    </div>
-                </div>
             </div>
         )
     );
