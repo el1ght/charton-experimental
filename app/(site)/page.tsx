3deb627228a56c590575312edb1d248a0809d3cc
el1ght <el1ghtifyoucan@gmail.com>
2024-11-15 20:15:23 +0200

    hotfix

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,24 +1,7 @@
 import UserDataLayout from "@/components/UserDataLayout";
 
-const mockSongs = [
-  {
-    id: '1',
-    user_id: '42545235',
-    author: 'Arctic Monkeys',
-    title: 'Arabella',
-    image_path: 'string'
-  },
-  {
-    id: '2',
-    user_id: '52452355',
-    author: 'Arctic Monkeys',
-    title: '505',
-    image_path: 'string'
-  },
-]
-
 export default function Home() {
   return (
-      <UserDataLayout songs={mockSongs} />
+      <UserDataLayout />
   );
 }
--- components/UserDataLayout.tsx
+++ components/UserDataLayout.tsx
@@ -20,13 +20,30 @@ interface UserData {
     songs: object;
 }
 
-const UserDataLayout = (songs: any) => {
+const mockSongs = [
+    {
+        id: '1',
+        user_id: '42545235',
+        author: 'Arctic Monkeys',
+        title: 'Arabella',
+        image_path: 'string'
+    },
+    {
+        id: '2',
+        user_id: '52452355',
+        author: 'Arctic Monkeys',
+        title: '505',
+        image_path: 'string'
+    },
+]
+
+const UserDataLayout = () => {
     const [telegramId, setTelegramId] = useState<string | null>(null)
     const [userData, setUserData] = useState<UserData | null>(null)
     const [hash, setHash] = useState<string | null>(null);
     const WebApp = useWebApp();
 
-    const onPlay = useOnPlay(songs!);
+    const onPlay = useOnPlay(mockSongs!);
 
     useEffect(() => {
         if (!WebApp) return;
@@ -59,74 +76,7 @@ const UserDataLayout = (songs: any) => {
 
     return (
         userData ? (
-            <div className={'p-5 box'}>
-                <div className={'flex justify-center items-center flex-col gap-y-1'}>
-                    <div className={'p-1.5 border-2 border-[#ddd] bg-[#424242]/[.3] rounded-3xl'}>
-                        <Image className={'w-[100px] h-[100px] rounded-3xl'} src={'/images/favBg-4.jpg'} alt={'image'} width={50} height={50} />
-                    </div>
-
-                    <h2 className={'text-center text-[1.2rem] font-bold mb-4'}>Home</h2>
-                </div>
-
-                <div className="mb-3">
-                    <h1 className="text-[#585858] text-[0.8rem]">
-                        Welcome Home
-                    </h1>
-                    <h2 className="text-black text-lg font-semibold">
-                        {userData.first_name}, you may like this
-                    </h2>
-                </div>
-
-                {/*<div className="grid grid-rows-8 grid-cols-5 gap-[.1rem] h-[100vh] box mb-3">*/}
-                {/*    <LikedWidget name={'Liked Songs'} href={'/liked'} />*/}
-                {/*    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden rounded-br-none'}>*/}
-                {/*        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[url('../public/images/favBg.jpg')] overflow-hidden hover:saturate-150 transition top-left "}>*/}
-                {/*            <div className={'relative h-full w-full bg-black/[.3]'}>*/}
-                {/*                <p className={'truncate text-white text-left m-4 mb-0 text-md font-semibold'}>World Top</p>*/}
-                {/*                <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}>Lana Del Rey, 21 Savage, Arctic Monkeys</p>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*        <div className={'absolute right-[10px] bottom-[10px] rounded-full'}>*/}
-                {/*            <FaPlay size={16} className={'text-black'} />*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*    <div className={'relative row-span-6 col-span-2 rounded-3xl overflow-hidden'}>*/}
-                {/*        <div className={"group flex items-center h-full bg-cover bg-center bg-[url('../public/images/favBg-5.jpg')] overflow-hidden hover:saturate-150 transition"}>*/}
-                {/*            <div className={'relative h-full w-full bg-black/[.3] rounded-3xl flex flex-col items-center justify-center'}>*/}
-                {/*                <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-semibold'}>TRY</p>*/}
-                {/*                <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-semibold'}>SMTH</p>*/}
-                {/*                <p className={'truncate text-white text-[1.5rem] max-w-[100px] font-semibold'}>NEW</p>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden rounded-br-none'}>*/}
-                {/*        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[url('../public/images/favBg-4.jpg')] overflow-hidden hover:saturate-150 transition top-left"}>*/}
-                {/*            <div className={'relative h-full w-full bg-black/[.3]'}>*/}
-                {/*                <p className={'truncate text-white text-left m-4 mb-0 text-md font-semibold'}>Greatest Hits</p>*/}
-                {/*                <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}>Lana Del Rey, 21 Savage, Arctic Monkeys</p>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*        <div className={'absolute right-[10px] bottom-[10px] rounded-full'}>*/}
-                {/*            <FaPlay size={16} className={'text-black'} />*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*    <div className={'relative row-span-2 col-span-3 rounded-3xl overflow-hidden rounded-br-none'}>*/}
-                {/*        <div className={"relative group flex items-center h-full bg-cover bg-center bg-[url('../public/images/favBg-3.jpg')] overflow-hidden hover:saturate-150 transition top-left"}>*/}
-                {/*            <div className={'relative h-full w-full bg-black/[.3]'}>*/}
-                {/*                <p className={'truncate text-white text-left m-4 mb-0 text-md font-semibold'}>Top Artists</p>*/}
-                {/*                <p className={'truncate text-white text-left ml-4 text-[0.6rem] max-w-[100px]'}>Arctic Monkeys, 21 Savage, Lana Del Rey</p>*/}
-                {/*            </div>*/}
-                {/*        </div>*/}
-                {/*        <div className={'absolute right-[10px] bottom-[10px] rounded-full'}>*/}
-                {/*            <FaPlay size={16} className={'text-black'} />*/}
-                {/*        </div>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
-
-            </div>
-        ) : (
             <div className={'relative px-3 py-5 pb-20 box flex flex-col gap-y-8'}>
-                {/*Loading...*/}
 
                 <div className={'bg-[#0e0e0e] p-3 rounded-xl flex justify-between'}>
                     <div className={'flex gap-x-2 items-center'}>
@@ -327,7 +277,7 @@ const UserDataLayout = (songs: any) => {
                     </div>
                     <div className={'flex flex-col gap-y-1'}>
                         {
-                            songs!.map((item: any) => (
+                            mockSongs!.map((item: any) => (
                                 <SongItem
                                     key={item.id}
                                     onClick={(id: string) => onPlay(id)}
@@ -488,6 +438,10 @@ const UserDataLayout = (songs: any) => {
                     <p className={'text-[12px]'}>Try out this smart (common) search</p>
                 </div>
 
+            </div>
+        ) : (
+            <div className={'h-full px-3 py-5'}>
+                Loading...
             </div>
         )
     );
